2f4e0e9526e0c260cfe6a8a3c7784a70b51cb417
aniekan7132 <saniekan32@gmail.com>
2022-12-15 13:48:11 -0800

    Sign

---
+++ 0x02-functions_nested_loops/4-main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - chacek the code.
+ *
+ * Return: Always 0.
+ */
+int _isalpha(int c)
+{
+	int r;
+
+	r = _isalpha('H');
+	_putchar(r + '0');
+	r = _isalpha('o');
+	_putchar(r + '0');
+	r = _isalpha(108);
+	_putchar(r + '0');
+	r = _isalpha(';');
+	_putchar(r + '0');
+	_putchar('\n');
+	return (0);
+}
