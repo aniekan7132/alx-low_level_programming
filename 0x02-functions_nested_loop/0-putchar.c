843a52d8c5c1877161d0d49a6fd50a966a697881
aniekan7132 <saniekan32@gmail.com>
2022-12-15 04:35:00 -0800

    0-putchar

--- 0x02-functions_nested_loop/0-putchar.c
+++ 0x02-functions_nested_loop/0-putchar.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Entry point
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 
 int main(void)
@@ -12,9 +12,9 @@ int main(void)
 	_putchar('u');
 	_putchar('t');
 	_putchar('c');
+	_putchar('h');
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
 	return (0);
-
 }
